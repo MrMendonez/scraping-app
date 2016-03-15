afaea3b195ff6743e231c57c5b9eef0d89977621
MrMendonez <dmendonez@gmail.com>
2016-03-15 15:54:42 -0400

    revamped routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -2,33 +2,14 @@ var controller = require('../controllers/controller.js');
 
 module.exports.routes = function(app) {
 
-  var request = require('request'); // gives us the ability to make web requests
-  var cheerio = require('cheerio'); // scrapes - allows us to use jquery syntax to parse a lot of data
-  var mongoose = require('mongoose');
-  var db = mongoose.connection;
-  var databaseUrl = "scraping-app";
-  var collections = ["insertedArticle"];
-
-  var Note = require('../models/Note.js');
-  var Article = require('../models/Article.js');
-
   app.get('/', controller.home);
 
-  //New Note Creation
-  app.post("/submit", controller.submit);
+  app.get("/scrape", controller.scrapeData);
 
-  //Route to see notes we have added
-  app.get('/notes', controller.notes);
+  app.post('/getNotes', controller.getNotes);
 
-  // Display scraped data
-  app.get('/displayInfo', controller.displayInfo);
+  app.post('/addNote', controller.addNote);
   
-  // app.post('/addNote/:id' , function(req, res) {
-       
-  // });
-  
-  // app.get('/delete/:id', function(req, res) {
-      
-  // });
+  app.post('/delete', controller.deleteNote);
     
 };
